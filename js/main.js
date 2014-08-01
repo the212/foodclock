5e99ec0e7795abe8a5d4e6fe7797545c53c59e53
Sebastian Soler <sebastian.a.soler@gmail.com>
2014-08-01 11:31:32 -0400

    added image arrays

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 $(function() {
+
+	var breakfast = ['images/1-breakfast/foodishfetish-tres-leches-doughnut-plant.jpg'];
+
+	var coffee = ['images/2-coffee/buvette-ny-cap-croissant.jpg', 'images/2-coffee/daniel-krieger-veselka.jpg'];
+
+	var lunch = ['images/3-lunch/Harris-Ueng-eisenbergs-corned-beef-matzo-ball-soup.jpg', 'images/3-lunch/robyn-lee-russ-daughters-lunch.jpg'];
+
   // Handler for .ready() called.
   // Create two variable with the names of the months and days in an array
 	var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]; 
@@ -25,11 +32,85 @@ $(function() {
 		// Add a leading zero to the minutes value
 		$("#min").html(( minutes < 10 ? "0" : "" ) + minutes);
 		},1000);
-		
+
+	var main_pic = 'images/3-lunch/Harris-Ueng-eisenbergs-corned-beef-matzo-ball-soup.jpg';
+
+	/****** dynamic pic **************/
+	var hour = new Date().getHours();
+
+	if (hour > 12) {
+		hour -= 12;
+		switch(hour) {
+			case 12:
+			case 13:
+			case 14:
+				//lunch
+				main_pic = coffee[Math.floor(Math.random()*coffee.length)];
+			break;
+
+			case 15:
+			case 16:
+			case 17:
+				// afternoon snack
+			break;
+
+			case 18:
+			case 19:
+			case 20:
+			case 21:
+				// dinner
+			break;
+
+			case 22:
+			case 23:
+			case 24:
+				// late night
+			break;
+
+		}
+	} else if (hour === 0) {
+		hour = 12;
+	} else {
+		switch(hour) {
+
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+				// sleep time
+				var item = items[Math.floor(Math.random()*items.length)];
+			break;
+
+			case 5:
+			case 6:
+			case 7:
+				//coffee
+				main_pic = coffee[Math.floor(Math.random()*coffee.length)];
+			break;
+
+			case 8:
+			case 9:
+			case 10:
+				// breakfast
+				main_pic = breakfast[Math.floor(Math.random()*breakfast.length)];
+			break;
+
+			case 12:
+			case 11:
+				//lunch
+				main_pic = lunch[Math.floor(Math.random()*lunch.length)];
+			break;
+
+		}
+
+		changePic(main_pic);
+	}
+
 	setInterval( function() {
 		// Create a newDate() object and extract the hours of the current time on the visitor's
 		var hours = new Date().getHours();
 		var ampm = "am";
+
 		if (hours > 12) {
 			hours -= 12;
 			ampm = "pm";
@@ -39,5 +120,12 @@ $(function() {
 		// Add a leading zero to the hours value
 		$("#hours").html(( hours < 10 ? "0" : "" ) + hours);
 		$("#ampm").html(ampm);
+
 		}, 1000);
 });
+
+function changePic(main_pic) {
+	console.log("main pic: ", main_pic);
+
+	$("html").css('background', "url(" + main_pic + ") no-repeat center center fixed");
+}
